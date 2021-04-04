359c05a729643e9c563e807a0cd42dcf89c2534e
sayin47 <sayinmehmet47@gmail.com>
2021-04-05 00:00:50 +0300

    added voice files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
   let session = $('#session-length').text();
   let sessionNum = Number(session);
+  let breaking=$('#break-length').text()
   let isPaused = true;
   let time = 0;
   let countdownTime = Number(sessionNum * 60 * 1000);
@@ -9,16 +10,47 @@
     if (!isPaused) {
       countdownTime -= 1000;
       let min = Math.floor(countdownTime / (60 * 1000));
-      let sec = Math.floor((countdownTime - min * 60 * 1000) / 1000);
+      let minLenght=min.toString().length
+      if(minLenght===1){
+        min=`0${min}`
+      }
+      let sec = Math.floor((countdownTime - min * 60 * 1000) / 1000)
+      let secLenght=sec.toString().length
+      if(secLenght===1){
+        sec=`0${sec}`
+      }
 
-      if (countdownTime <= 0);
-      else $('#time-left').html(`${min}:${sec}`);
+      $('#time-left').html(`${min}:${sec}`);
+      if(countdownTime<=0){
+        $("#beep")[0].play()
+        if($('#timer-label').text()==="Session"){
+        
+          countdownTime=uiBreak.length*1000*60+1000
+        
+          $('#time-left').html(`${min}:${sec}`)
+          setTimeout(() => {
+            $('#timer-label').text("Break")
+          }, 1000);
+         
+
+       
+
+        }else{
+          countdownTime=uiSession.length*1000*60+1000
+          $('#time-left').html(`${min}:${sec}`)
+          setTimeout(() => {
+            $('#timer-label').text("Session")
+          }, 1000);
+        
+        }  
+      }
     }
   }, 1000);
 
   $('#start_stop').click(function (e) {
     isPaused = !isPaused;
-  
+   
+   
   });
 
 
