0b39092f5aee62e3b485dff348d1fd03cc0c77a9
sayin47 <sayinmehmet47@gmail.com>
2021-06-02 15:59:15 +0300

    solved some bugs

--- script.js
+++ script.js
@@ -2,29 +2,26 @@ let session = $('#session-length').text();
 let sessionNum = Number(session);
 let breaking = $('#break-length').text();
 let isPaused = true;
-var doubleClicked=false;
+var doubleClicked = false;
 let time = 0;
 let countdownTime = Number(sessionNum * 60 * 1000);
 
 let timer = setInterval(() => {
   if (!isPaused) {
-
     countdownTime -= 1000;
-    doubleClicked=false;
+    doubleClicked = false;
     breaking = $('#break-length').text();
-    
 
     let min = Math.floor(countdownTime / (60 * 1000));
-    let minLenght = min.toString().length;
-    if (minLenght === 1) {
+    let minLength = min.toString().length;
+    if (minLength === 1) {
       min = `0${min}`;
     }
     let sec = Math.floor((countdownTime - min * 60 * 1000) / 1000);
-    let secLenght = sec.toString().length;
-    if (secLenght === 1) {
+    let secLength = sec.toString().length;
+    if (secLength === 1) {
       sec = `0${sec}`;
     }
-   
 
     $('#time-left').html(`${min}:${sec}`);
     if (countdownTime <= 0) {
@@ -48,28 +45,24 @@ let timer = setInterval(() => {
 }, 1000);
 
 $('#start_stop').click(function (e) {
-   sessionNum = Number($('#session-length').text());
-   countdownTime = Number(sessionNum * 60 * 1000);
+  e.preventDefault();
+  // sessionNum = Number($('#session-length').text());
+  // countdownTime = Number(sessionNum * 60 * 1000);
 
   isPaused = !isPaused;
-      $(".fa-play").toggleClass("d-none");
-      $(".fa-pause").toggleClass("d-none");
+  console.log(sessionNum)
+  isPaused ? $('#beep')[0].pause() : null;
 
+  $('.fa-play').toggleClass('d-none');
+  $('.fa-pause').toggleClass('d-none');
 
-    
-  
-
-  doubleClicked=false;
-
+  doubleClicked = false;
 });
 
-
-$(".fa-volume-mute").click(e=>{
-  e.preventDefault()
-  $('#beep')[0].pause()
-
-})
-
+$('.fa-volume-mute').click((e) => {
+  e.preventDefault();
+  $('#beep')[0].pause();
+});
 
 //buttons increase decrease the time
 $('#break-increment').click(function (e) {
@@ -112,21 +105,20 @@ function toggleFullScreen() {
 }
 
 $('.session').click(function (e) {
-
-  doubleClicked=true
+  doubleClicked = true;
 
   if (e.target.classList.contains('changeable')) {
     e.target.setAttribute('contenteditable', true);
     if (e.target.textContent === 'Click to translate') {
-      e.target.textContent = "";
+      e.target.textContent = '';
       e.target.focus();
     }
   }
 });
 
 $('.break').click(function (e) {
-  e.preventDefault()
-  doubleClicked=true
+  e.preventDefault();
+  doubleClicked = true;
 
   if (e.target.classList.contains('changeable')) {
     e.target.setAttribute('contenteditable', true);
@@ -136,16 +128,11 @@ $('.break').click(function (e) {
   }
 });
 
+$('html').on('click', function (e) {
+  e.preventDefault();
 
-
-$("html").on("click",function(e){
-  e.preventDefault()
-
-  if(doubleClicked===true){
+  if (doubleClicked === true) {
     uiSession.input();
-    uiBreak.input()
-
+    uiBreak.input();
   }
-
-})
-
+});
